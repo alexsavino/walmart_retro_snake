b5c6eb98ac03e60500c612b874f74c7d8e7946a9
Alexandra Savino <avs2167@barnard.edu>
2023-06-07 19:55:05 -0400

    big update

--- pygame_snake.py
+++ pygame_snake.py
@@ -4,10 +4,15 @@
 # @Author:  ALEXANDRA SAVINO 
 # @Email:   avs2167@barnard.edu
 
+# Legend:
+#   '# ?? ___' = something to be revisited, a question
+#   '# PLACEHOLDER' = a placeholding value of some kind
+
 import pygame as pyg
 from pygame.locals import *
 import numpy as np
 import random as rand
+import sys
 '''
 import sys
 sys.stdout = open('output.log', 'w')
@@ -18,19 +23,28 @@ pyg.init()
 width, height = 640, 480
 screen = pyg.display.set_mode((width,height))
 gen_col = 'white'  # gen_option_sc_txt_color
-
+clock = pyg.time.Clock()
 
 # INITIALIZING SCREENS...
-title_screen = True
+title_screen = False
 option_screen = False
-game_screen = False
+game_screen = True
 final_screen = False
-#PREVIOUS_PAGE = title_screen # is this format / connection builder im rly gonna use?
 
 # TO CREATE MAC DISPLAY NAME IN THE CASE OF NO USERNAME...
 animal_list = ['Fox','Robin','Chipmunk','Squirrel','Deer']
 round_animal_name = rand.choice(animal_list)
     
+# INSTANTIATING THE ARRAY SQUARE PROPERTIES...
+square_size = 18
+row_span = (width-75)
+col_span = (height-100)
+num_rows = col_span // square_size
+num_cols = row_span // square_size
+
+# TO POSITION THE ARRAY...
+board_x = (width - (num_cols * square_size)) / 2
+board_y = 78
 
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -101,10 +115,11 @@ def color_bar_caption(color, q_top):
     screen.blit(color, color_rect)
 
 
+color_options = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE']
 def color_bar_system(q_rect, q_top, line_identifier):
     global hovered_colors
-
-    color_options = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE']
+    global snake_color, pellet_color, board_color
+    global color_options
 
     for color_index in range(len(color_options)):
         # DRAWING THE COLORED RECTANGLES...
@@ -135,23 +150,62 @@ def color_bar_system(q_rect, q_top, line_identifier):
         last_hovered_box = pyg.Rect(last_hovered_box_x, last_hovered_box_y, color_box_width, color_box_width)
         pyg.draw.rect(screen, gen_col, last_hovered_box, width=2)
 
+        if line_identifier == 'snake':
+            snake_color = hovered_colors[line_identifier]
+            return snake_color
+        elif line_identifier == 'pellet':
+            pellet_color = hovered_colors[line_identifier]
+            return pellet_color
+        elif line_identifier == 'board':
+            board_color = hovered_colors[line_identifier]
+            return board_color
+
 hovered_colors = {}
 
 
+def get_pellet(pellet_color):
+    global board_x, board_y
+    pellet_x = board_x + (rand.randint(1,num_cols-1) * square_size)
+    pellet_y = board_y + (rand.randint(1,num_rows-1) * square_size)
+    pyg.draw.rect(screen, pellet_color, 
+        (pellet_x, pellet_y, square_size, square_size))
+    
+
+def draw_border_lines():
+    global line_1, line_2, line_3, line_4
+
+    # Top line...
+    line_1 = (board_x, board_y - 1, width - board_x, board_y - 1)
+    # Left line...
+    line_2 = (board_x - 1, board_y, board_x - 1, board_y + col_span - 3)
+    # Right line...
+    line_3 = (width - board_x + 1, board_y, width - board_x + 1, board_y + col_span - 3)
+    # Bottom line...
+    line_4 = (board_x, board_y + col_span - 2, width - board_x, board_y + col_span - 2)
+
+    pyg.draw.line(screen, gen_col, line_1[0:2], line_1[2:4], width=1)
+    pyg.draw.line(screen, gen_col, line_2[0:2], line_2[2:4], width=1)
+    pyg.draw.line(screen, gen_col, line_3[0:2], line_3[2:4], width=1)
+    pyg.draw.line(screen, gen_col, line_4[0:2], line_4[2:4], width=1)
+
+
+def check_snake_collision(snake_segments):
+    global game_screen, final_screen
+    x, y = snake_segments[0]
+    snake_head_rect = pyg.Rect(x, y, square_size, square_size)
+
+    if snake_head_rect.clipline(line_1[0:2], line_1[2:4]) or \
+       snake_head_rect.clipline(line_2[0:2], line_2[2:4]) or \
+       snake_head_rect.clipline(line_3[0:2], line_3[2:4]) or \
+       snake_head_rect.clipline(line_4[0:2], line_4[2:4]):
+        print('cOlLision AH!')
+        #game_screen = False
+        #final_screen = True
 
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # PRIMARY GAME LOOP...
 while True:
-<<<<<<< HEAD
-    #title_screen = False
-    #option_screen = False
-    #game_screen = True
-=======
-    title_screen = True
-    option_screen = False
-    game_screen = False
->>>>>>> c8bc4765055efaba84001eb79d1e89462962d1c1
 
     SPACE_timer = 0
 
@@ -318,8 +372,10 @@ while True:
 
     username = username.strip()
 
-# maybe i can make it so that when you do hit enter on your name quotes appear? 
-#   maybe a color change? put a white box ar    ound it? something to differentiate
+# ?? HOW TO MAKE IT SO THAT SOMEONE'S NAME STAYS EVEN AS YOU 
+#    GO BACK AND FORTH BETWEEN OPTION AND GAME SCREENS??
+# ?? maybe i can make it so that when you do hit enter on your name quotes appear? 
+#   maybe a color change? put a white box around it? something to differentiate
 #   how could you go back and edit it yourself?
 
 
@@ -327,16 +383,43 @@ while True:
     # TO CREATE MAC DISPLAY NAME IN THE CASE OF NO USERNAME...
     anonymous_username = pyg.display.set_caption("Anonymous {}'s Snake Game".format(round_animal_name))
                                                  
-    TEST_USER_COLOR = 'red'
+    TEST_BOARD_COLOR = 'PURPLE' # PLACEHOLDER ... supposed to be 'board_color'
+    snake_color = 'BLUE' # TEST_SNAKE_COLOR
+    TEST_PELLET_COLOR = 'RED'
 
+    # TO INSTANTIATE THE SNAKE...
+    snake_box_length = 3   # ?? IS THIS SOMETHING TO PULL OUT OF THIS WHILE LOOP?
+    snake_length = snake_box_length * square_size
+    snake_width = square_size
+
+    snake_x = board_x + (10 * square_size)
+    snake_y = board_y + (10 * square_size)
+    snake_direction = 'right'
+    snake_segments = []
+
+    for square in range(snake_box_length):
+        segment_x = snake_x - (snake_box_length - square) * square_size
+        segment_y = snake_y
+        snake_segments.append((segment_x, segment_y))
+
+    snake_segments = snake_segments[::-1]
+
+    # TO DETERMINE BOTH BOARD COLORS...
     board_colors = [['red','tomato'],['chocolate','orange'],['gold','yellow'],
         ['green','limegreen'],['blue','royalblue'],['darkviolet','mediumorchid']]
     
+    board_color_1 = board_colors[color_options.index(TEST_BOARD_COLOR)][0]
+    board_color_2 = board_colors[color_options.index(TEST_BOARD_COLOR)][1]
 
+    # TO INSTANTIATE THE FIRST 
+    pellet_counter = 0
+    get_pellet(TEST_PELLET_COLOR)
 
+    
     while game_screen:
         screen.fill('black')
         mouse_pos = pyg.mouse.get_pos()
+        clock.tick(10)
 
         # TO CREATE MAC DISPLAY NAME...
         if username == "":
@@ -346,7 +429,7 @@ while True:
 
         # TO CREATE SCREEN-BUTTONS!
         back_arrow_polygon_option = back_arrow()
-        #forward_arrow_polygon_option = forward_arrow()
+        # ?? the options will be to either 1. quit the game and go to the option screen ....
 
         # SCREEN TITLE...
         game_title_font = pyg.font.Font(None,50)
@@ -355,55 +438,18 @@ while True:
         game_title_rect = game_title.get_rect(center=(width//2,game_title_height))
         screen.blit(game_title, game_title_rect)
 
-
-
-        square_size = 18
-        row_span = (width-75)
-        col_span = square_size#(height-100)
-        num_rows = col_span // square_size
-        num_cols = row_span // square_size
-
-        # Define the two colors for the checkered pattern
-        color1 = 'darkviolet'
-        color2 = 'mediumorchid'
-
-        #print('width: ', width)
-        #print('row_span: ', row_span)
-        board_x = (width - (num_cols * square_size)) / 2
-        board_y = 78
-        
-        # Loop over the rows and columns to draw the squares
+    
+        # TO DRAW THE ARRAY...
         for row in range(num_rows):
-            x = board_x + (col * square_size)
-            y = board_y + (row * square_size)
             for col in range(num_cols):
-                x = board_x + (col * square_size)
-                y = board_y + (row * square_size)
-                color = color1 if (row + col) % 2 == 0 else color2
+                x = board_x + col * square_size
+                y = board_y + row * square_size
+                color = board_color_1 if (row + col) % 2 == 0 else board_color_2
                 pyg.draw.rect(screen, color, (x, y, square_size, square_size))
 
-        #pyg.draw.line(screen,gen_col,(board_x,board_y),(width - board_x,board_y),width=1)
-        #pyg.draw.line(screen,gen_col,(board_x,board_y),(board_x,board_y,width=1)
-        #pyg.draw.line(screen,gen_col,(,),(,),width=1)
-        #pyg.draw.line(screen,gen_col,(,),(,),width=1)
-
-
-        '''
-        for row in range(num_rows+1):
-            # TO DRAW HORIZONTAL WHITE LINES...
-            x_i = board_x
-            y = board_y + (row * square_size)
-            x_f = width - board_x
-            pyg.draw.line(screen, gen_col, (x_i,y),(x_f,y), width=1)
-
-            for col in range(num_cols+1):
-                # TO DRAW VERTICAL WHITE LINES...
-                x = board_x + (col * square_size)
-                y_i = board_y
-                y_f = board_y + (row * square_size)
-                pyg.draw.line(screen, gen_col ,(x,y_i),(x,y_f), width=1)
-        '''
-
+        
+        # TO DRAW THE SNAKE...
+        snake_segments.pop()
 
         # EVENTS CATCHER...
         for event in pyg.event.get():
@@ -419,7 +465,53 @@ while True:
                     game_screen = False
                     break
             
+            elif event.type == KEYDOWN:
+                if event.key == pyg.K_UP and snake_direction != 'down':
+                    snake_direction = 'up'
+                elif event.key == pyg.K_DOWN and snake_direction != 'up':
+                    snake_direction = 'down'
+                elif event.key == pyg.K_LEFT and snake_direction != 'right':
+                    snake_direction = 'left'
+                elif event.key == pyg.K_RIGHT and snake_direction != 'left':
+                    snake_direction = 'right'
+
+        # TO CALCULATE NEW HEAD SQUARE + SHIFT UP OTHER SQUARES...
+        if snake_direction == 'up':
+            new_segment_x = snake_segments[0][0]
+            new_segment_y = snake_segments[0][1] - square_size
+        elif snake_direction == 'down':
+            new_segment_x = snake_segments[0][0]
+            new_segment_y = snake_segments[0][1] + square_size
+        elif snake_direction == 'left':
+            new_segment_x = snake_segments[0][0] - square_size
+            new_segment_y = snake_segments[0][1]
+        elif snake_direction == 'right':
+            new_segment_x = snake_segments[0][0] + square_size
+            new_segment_y = snake_segments[0][1]
+
+        snake_segments.insert(0, (new_segment_x, new_segment_y))
+
+
+        for segment in snake_segments:
+            x, y = segment
+            pyg.draw.rect(screen, snake_color, (x, y, square_size, snake_width))
+
+        draw_border_lines()
+        check_snake_collision(snake_segments)
+
         pyg.display.flip()
+
+        # ?? maybe the snake itself can be rounded at its head / tail? .. take up 1/2 box
+         
+        # ?? WE NEED TO INCLUDE AN PELLET!!!! COUNTER!!!!
+        #   (WE NEED TO INCLUDE A TIMER?
+
+        # ?? WE HAVE TO RANDOMIZE WHERE THE FIRST PELLET GOES AND WHERE THEY GO!
+        #   THEY JUST CAN'T SPAWN ON TOP OF THE SNAKE
+
+        # ?? WE NEED TO PUT BOUNDARIES SO THAT THE SNAKE CRASHES INTO WALLS / ITSELF
+
+        # LENGHT COUNTER???
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     # this is what should be presented SHOULD A GAME BE FINISHED!
